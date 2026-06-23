cb9c566b891d8ecf6ffc2e344f19ac0e05f8c9f4
greenfig759 <1004skfro7@gmail.com>
2026-06-23 20:16:34 +0900

    init project

---
+++ src/main/java/com/mart/quickpass/QuickPassApplication.java
@@ -0,0 +1,13 @@
+package com.mart.quickpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickPassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mart/quickpass/QuickPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mart.quickpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
